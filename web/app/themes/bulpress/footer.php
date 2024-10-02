9acad39f9ff525ee750bf9c304fa488856f14c78
Hammad Majid <hammadmajid@proton.me>
2024-10-03 00:28:36 +0500

    create footer

---
+++ web/app/themes/bulpress/footer.php
@@ -0,0 +1,17 @@
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>Bulpress</strong> by <a href="https://bine.pages.dev">Hammad Majid</a>.
+      The source code is licensed
+      <a href="https://opensource.org/license/mit">MIT</a>.
+  </div>
+</footer>
+
+<?php
+  wp_footer();
+?>
+
+
+</body>
+
+</html>
