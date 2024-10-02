9640e5ba7742b5b4fda51ea85104d14a45905547
Hammad Majid <hammadmajid@proton.me>
2024-10-03 00:29:24 +0500

    create hero section on front page

---
+++ web/app/themes/bulpress/front-page.php
@@ -0,0 +1,17 @@
+<?php
+    get_header();
+?>
+
+<section class="hero"> 
+    <div class="hero-body">
+        <h1 class="title">Hello, world!</h1>
+        <div>
+            <button class="button is-primary">Get started <i class="fa-solid fa-bars"></i></button>
+            <button class="button">Learn more</button>
+        </div>
+    </div>
+    </section>
+
+<?php
+    get_footer();
+?>
