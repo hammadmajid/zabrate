a4123a1a6612cdb56ad8751cba02aeb937986203
Hammad Majid <hammadmajid@proton.me>
2024-10-03 00:28:18 +0500

    create 404 page

---
+++ web/app/themes/bulpress/404.php
@@ -0,0 +1,22 @@
+<?php
+get_header();
+
+?>
+
+<main class="hero is-medium">
+    <div class="hero-body">
+        <h1 class="title">404</h1>
+        <p class="subtitle">Page not found</p>
+        <a href="/" class="button is-primary">
+            <span class="icon is-small">
+                <i class="fas fa-home"></i>
+            </span>
+            <span>Go Home</span>
+        </a>    
+    </div>
+</main>
+
+<?php
+
+get_footer();
+?>
